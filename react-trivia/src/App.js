c00892640524e6566f43617ce691b6a447bcba34
Laura Quantrille <lquantrille@gmail.com>
2021-03-23 13:42:30 -0400

    imported tachyons, selectable categories in place

--- react-trivia/src/App.js
+++ react-trivia/src/App.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import CategoryList from './components/CategoryList'
+import 'tachyons'
 // import './App.css';
 
 function App () {
   const [categories, setCategories] = useState([])
+  const [SelectedCategory, setSelectedCategory] = useState(null)
   useEffect(() => {
     console.log('categories runs', categories)
     axios.get('https://opentdb.com/api_category.php').then((response) => {
@@ -13,16 +16,16 @@ function App () {
   console.log('RENDERING:', categories)
 
   return (
-    <div className='App'>
-      <h1>Category is...</h1>
-      <ul>
-        {categories.map((category, index) => (
-          <li key={index}>{category.name}</li>
-        ))}
-      </ul>
+    <div>
+      <h1>Trivia Game</h1>
+      <CategoryList 
+        categories={categories}
+        setSelectedCategory={setSelectedCategory}
 
+       />
     </div>
-  )
+
+    )
 }
 
 export default App
