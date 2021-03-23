e4ca7db20485dfdfe1376adf2676470e54ae12de
Laura Quantrille <lquantrille@gmail.com>
2021-03-23 14:53:47 -0400

    go back button working, specific category rendering on click

--- react-trivia/src/App.js
+++ react-trivia/src/App.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import CategoryList from './components/CategoryList'
+import CategoryQuestions from './components/CategoryQuestions'
 import 'tachyons'
 // import './App.css';
 
 function App () {
   const [categories, setCategories] = useState([])
-  const [SelectedCategory, setSelectedCategory] = useState(null)
+  const [selectedCategory, setSelectedCategory] = useState(null)
   useEffect(() => {
     console.log('categories runs', categories)
     axios.get('https://opentdb.com/api_category.php').then((response) => {
@@ -18,11 +19,19 @@ function App () {
   return (
     <div>
       <h1>Trivia Game</h1>
+      {selectedCategory ? (
+        <CategoryQuestions category={selectedCategory} 
+        handleGoBack={() => setSelectedCategory(null)}
+          />
+      ) : (
       <CategoryList 
         categories={categories}
         setSelectedCategory={setSelectedCategory}
 
+        
        />
+       
+       )}
     </div>
 
     )
---
+++ react-trivia/src/components/CategoryQuestions.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function CategoryQuestions({ category, handleGoBack }) {
+    const [categoryQuestions, setCategoryQuestions] = useState([])
+    useEffect(() => {
+        axios
+          .get(`https://opentdb.com/api.php?amount=${category.id}`)
+          .then((response) => {
+              console.log(response)
+            //   const data = response.data.map()
+          })
+    })
+  return (
+    <div>
+        <h3>{category.name} Questions</h3>
+        <button
+            className="pa0 bw0 bg-white blue pointer underline-hover"
+            onClick={handleGoBack}
+          >
+            Back to all categories
+          </button>
+            <ul>
+
+            </ul>
+        </div>
+    )
+}
